ea993504f128efb193525ef16791caaa893b054e
Elena Iurcenko <lena.iurcenko@mail.ru>
2023-08-08 08:28:04 +0300

    Шаблон калькулятора

---
+++ calculator.py
@@ -0,0 +1,12 @@
+
+class Calculator:
+    last_res = None          #переменная по-умолчанию равна нулю
+    def sum(self ,n1, n2):       #функция сумма принимает два числа
+        self.last_res = n1 + n2
+        return n1+n2             #возвращает результат
+
+
+    def print_last_res(self):
+        print(self.last_res)   #вызывает функцию
+
+calculator = Calculator()        
---
+++ test_calculator.py
@@ -0,0 +1,6 @@
+from calculator import Calculator
+
+calc = Calculator()    #вызов функции
+calc.sum(10, 5)        #сумирование
+calc.print_last_res()  #принт
+# >>15
