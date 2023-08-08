38f00e7df26c86bffaf9ed78d6151c8949cfa943
Elena Iurcenko <lena.iurcenko@mail.ru>
2023-08-08 15:10:12 +0300

    Task-3 Функция разности

--- calculator.py
+++ calculator.py
@@ -1,4 +1,6 @@
 import traceback
+
+#Это наш Калькулятор
 class Calculator:
     last_res = None          #переменная по-умолчанию равна нулю
     def sum(self ,n1, n2):       #функция сумма принимает два числа
