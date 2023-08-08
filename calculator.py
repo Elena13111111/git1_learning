634c064d62519f665403b718bbbdcdb6944c07c3
Elena Iurcenko <lena.iurcenko@mail.ru>
2023-08-08 14:41:56 +0300

    Task-2 Функция деления

--- calculator.py
+++ calculator.py
@@ -1,4 +1,4 @@
-
+import traceback
 class Calculator:
     last_res = None          #переменная по-умолчанию равна нулю
     def sum(self ,n1, n2):       #функция сумма принимает два числа
@@ -8,6 +8,14 @@ class Calculator:
     def multiply(self, n1, n2):
         self.last_res = n1 * n2
         return n1 * n2
+
+    def divide(self, n1, n2):
+        try:
+            res = n1 / n2
+            self.last_res = res
+            return res
+        except:
+            traceback.print_exc()      #если ошибка будет
 
 
     def print_last_res(self):
